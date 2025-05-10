2e69466d20a0078514f7753eb258c51de3453fc7
Jake Pullen <Jake.Pullen333@gmail.com>
2025-05-10 16:34:20 +0100

    debug: 🧪 trying to link to stylesheet....

--- src/verity.py
+++ src/verity.py
@@ -5,8 +5,8 @@ import atexit
 
 from flask import Flask
 
-from data_handler import database
 from config import VerityConfig
+from data_handler import database
 from front.home import home_bp
 
 def set_up_logging(config):
@@ -18,15 +18,20 @@ def set_up_logging(config):
         atexit.register(queue_handler.listener.stop)
 
 if __name__ == "__main__":
+    # config and logging
     verity_config = VerityConfig()
     logger = logging.getLogger(__name__)
     set_up_logging(verity_config)
     logger.info('app starting')
+
+    # database initialise
     verity = database(verity_config)
     verity.build_database()
 
+    # app initialise
     app = Flask(__name__)
     app.config['SECRET_KEY'] = verity_config.SECRET_KEY
+    app.config['DEBUG'] = True
+    app.config['STATIC_FOLDER'] = 'front/static'
     app.register_blueprint(home_bp)
-
-    app.run(debug=True)
+    app.run()
